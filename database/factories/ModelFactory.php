a87c8cf616697ad1962c417ee5ffe986e1006d29
jesusmg10 <jesusmolinagarcia10@gmail.com>
2019-11-28 19:18:29 -0400

    modificacion1

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -8,7 +8,7 @@
 | Here you may define all of your model factories. Model factories give
 | you a convenient way to create models for testing and seeding your
 | database. Just tell the factory how a default model should look.
-|
+|esta es la modificacion de la que ahbilo
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
